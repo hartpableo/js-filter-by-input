57438702d46a7cec554f5efe96cb7df1a58214cd
Hart <pableoh@gmail.com>
2023-01-19 09:37:32 +0800

    styling progress

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,css}"],
+  theme: {
+    fontFamily: {
+      "primaryFont": "'Ubuntu', sans-serif"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
